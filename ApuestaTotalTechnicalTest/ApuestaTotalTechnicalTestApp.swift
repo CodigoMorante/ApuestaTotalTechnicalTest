c5f0b7f5a73e49e5f7d440b49aac18157aae079b
CodigoMorante <cris.cams.kira@gmail.com>
2025-10-22 14:10:18 -0500

    create the project

---
+++ ApuestaTotalTechnicalTest/ApuestaTotalTechnicalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApuestaTotalTechnicalTestApp.swift
+//  ApuestaTotalTechnicalTest
+//
+//  Created by Christian Morante on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ApuestaTotalTechnicalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
