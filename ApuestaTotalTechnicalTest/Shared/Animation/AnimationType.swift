6ccb916a17db47b923b742b5fa68ce19a495b008
CodigoMorante <cris.cams.kira@gmail.com>
2025-10-27 16:57:10 -0500

    improve enum AnimationType

--- ApuestaTotalTechnicalTest/Shared/Animation/AnimationType.swift
+++ ApuestaTotalTechnicalTest/Shared/Animation/AnimationType.swift
@@ -5,20 +5,10 @@
 //  Created by Christian Morante on 27/10/25.
 //
 
-enum AnimationType {
+enum AnimationType: String {
     case pulse, flash, sparkle, confetti, shine, explosion, bounce, crownBurst
     
-    init?(from string: String) {
-        switch string {
-        case "pulse": self = .pulse
-        case "flash": self = .flash
-        case "sparkle": self = .sparkle
-        case "confetti": self = .confetti
-        case "shine": self = .shine
-        case "explosion": self = .explosion
-        case "bounce": self = .bounce
-        case "crownBurst": self = .crownBurst
-        default: self = .flash
-        }
+    init?(from raw: String) {
+        self.init(rawValue: raw)
     }
 }
